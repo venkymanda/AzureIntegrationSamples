b8bdc517291f75ac847f9ec75ee033639243efb6
venkymanda <venkat.18power@gmail.com>
2023-12-21 11:57:19 +0530

    Add Service Bus Trigger Sample

--- Azure Functions/IsolatedExample/Function1.cs
+++ Azure Functions/IsolatedExample/Function1.cs
@@ -7,28 +7,27 @@ using Microsoft.Extensions.Logging;
 namespace FunctionApp2
 {
     public class Function1
-    {
+        {
         private readonly ILogger _logger;
         private readonly IKIKService _kikservice;
 
-        public Function1(ILoggerFactory loggerFactory,IKIKService kIKService)
-        {
-            _logger = loggerFactory.CreateLogger<KIKService>();
-            _kikservice = kIKService;
+            public Function1(ILoggerFactory loggerFactory,IKIKService kIKService)
+            {
+                _logger = loggerFactory.CreateLogger<KIKService>();
+                _kikservice = kIKService;
+            }
 
-        }
+            [Function("Function1")]
+            public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+            {
+                _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        [Function("Function1")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+                var response = req.CreateResponse(HttpStatusCode.OK);
+                response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+                _kikservice.HttpSendTest("te");
+                response.WriteString("Welcome to Azure Functions!");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-            _kikservice.HttpSendTest("te");
-            response.WriteString("Welcome to Azure Functions!");
-
-            return response;
-        }
+                return response;
+            }
     }
 }
---
+++ Azure Functions/ServiceBusTriggerFunction/ServiceBusTrigger.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Net.Http.Headers;
+using System.Net;
+using System.Text;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
+
+namespace ServiceBusTriggerFunction
+{
+    public class ServiceBusTrigger
+    {
+        private readonly ILogger<ServiceBusTrigger> _logger;
+        private readonly HttpClient _httpClient;
+
+        public ServiceBusTrigger(ILogger<ServiceBusTrigger> logger,
+                                IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        [Function(nameof(ServiceBusTrigger))]
+        public async Task<VoidResult> Run([ServiceBusTrigger("kafkacjvn", "kafkacjvn", Connection = "connectionstring")] ServiceBusReceivedMessage message)
+        {
+            _logger.LogInformation("Message ID: {id}", message.MessageId);
+            _logger.LogInformation("Message Body: {body}", message.Body);
+            _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+            try
+            {
+                // Get Bearer token
+                var accessToken = await GetAccessToken();
+
+                if (accessToken != null)
+                {
+                    _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.AccessToken);
+                }
+                string apiUrl = Environment.GetEnvironmentVariable("D365URL") ?? "";
+                HttpContent content = new StringContent(Encoding.UTF8.GetString(message.Body), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await _httpClient.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Message ID: {id} sent to D365 Succesfully", message.MessageId);
+                }
+
+                return new VoidResult();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Message failed with  {ex}");
+
+                throw new Exception(ex.Message);
+            }
+        }
+        public struct VoidResult { }
+        public async Task<AuthenticationResult> GetAccessToken()
+        {
+            var clientId = Environment.GetEnvironmentVariable("D365FO.ClientId");
+            var clientSecret = Environment.GetEnvironmentVariable("D365FO.ClientSecret");
+            var tenantId = Environment.GetEnvironmentVariable("D365FO.TenantId");
+            var resourceUri = Environment.GetEnvironmentVariable("D365FO.ResourceUri");
+            Uri uri = new Uri("https://login.microsoftonline.com/" + tenantId);
+
+            var app = ConfidentialClientApplicationBuilder.Create(clientId)
+                                                    .WithClientSecret(clientSecret)
+                                                    .WithAuthority(uri)
+                                                    .Build();
+
+            var authResult = await app.AcquireTokenForClient(
+                                            new[] { $"{resourceUri}/.default" })
+
+                                            .ExecuteAsync()
+                                            .ConfigureAwait(false);
+
+            return authResult;
+
+
+
+        }
+    }
+}
