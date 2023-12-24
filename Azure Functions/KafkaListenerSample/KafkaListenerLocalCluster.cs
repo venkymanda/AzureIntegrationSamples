6f67d4f4405e7f3acab9de468bd12c880dd5d05e
venkymanda <venkat.18power@gmail.com>
2023-12-24 16:10:10 +0530

    Updating Kafka Listner to Handle different Event Types based on header

--- Azure Functions/KafkaListenerSample/KafkaListenerLocalCluster.cs
+++ Azure Functions/KafkaListenerSample/KafkaListenerLocalCluster.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using System.Reflection.PortableExecutable;
+using System.Text;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -24,11 +26,66 @@ namespace KafkaListenerSample
              * 
              * Username = "ConfluentCloudUserName",//API access key from the Confluent
              * Password = "ConfluentCloudPassword",//API secret obtained from the Confluent
+             * Protocol = BrokerProtocol.SaslSsl,
+             * AuthenticationMode = BrokerAuthenticationMode.Plain,
              */
+                  Username = "ConfluentCloudUserName",//API access key from the Confluent
+                  Password = "ConfluentCloudPassword",//API secret obtained from the Confluent
+                  Protocol = BrokerProtocol.SaslSsl,
+                  AuthenticationMode = BrokerAuthenticationMode.Plain,
+
                   ConsumerGroup = "$Default")] string eventData, FunctionContext context)
         {
             var logger = context.GetLogger("KafkaFunction");
             logger.LogInformation($"C# Kafka trigger function processed a message: {JObject.Parse(eventData)["Value"]}");
+            try
+            {
+                // Deserialize the Kafka message
+                var kafkaMessage = JObject.Parse(eventData);
+
+                // Extract the event type from the message
+                if (kafkaMessage.TryGetValue("Headers", StringComparison.OrdinalIgnoreCase, out var eventTypeHeader))
+                {
+                    logger.LogInformation($"Received message with Event Type: {eventTypeHeader}");
+                    var eventType = GetHeaderValue(eventTypeHeader, "EventType");
+                    // Decode Base64-encoded event type
+                    var decodedEventType = Encoding.UTF8.GetString(Convert.FromBase64String(eventType));
+                    // Perform actions based on the event type
+                    switch (decodedEventType.ToString())
+                    {
+                        case "Kafka-ServiceBusEvent":
+                            logger.LogInformation($" message with Event Type is Handled Successfully: {decodedEventType}");
+                            break;
+
+                        default:
+                            // Handle unknown event types
+                            break;
+                    }
+                }
+                else
+                {
+                    logger.LogWarning("Received message without an event type.");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Error processing Kafka message: {ex.Message}");
+            }
         }
+
+        private static string GetHeaderValue(JToken headers, string key)
+        {
+            if (headers is JToken headersArray)
+            {
+                var header = headersArray.FirstOrDefault(h => h["Key"]?.ToString() == key);
+                if (header != null)
+                {
+                    return header["Value"]?.ToString();
+                }
+            }
+            return null;
+        }
+
+
     }
 }
